aa0edc5847c5e8cfffa01e92787faedde1873582
walonCode <mohamedlaminwalonjalloh@gmail.com>
2025-06-11 00:06:48 +0000

    edit added the cors configuration

--- server/booking-service/main.go
+++ server/booking-service/main.go
@@ -2,8 +2,9 @@ package main
 
 import (
 	"net/http"
+	"time"
 
-	// "github.com/gin-contrib/cors"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/waloncode/hotel_booking_app/routes"
 )
@@ -12,9 +13,14 @@ func main(){
 	app := gin.Default()
 
 	//middleware
-	// app.Use(cors.New(cors.Config{
-
-	// }))
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: []string{"http://localhost:3001/",},
+		AllowMethods:  []string{"GET","POST","PATCH","DELETE","PUT"},
+		AllowHeaders: []string{"Origin","Content-Type","Authorization"},
+		ExposeHeaders: []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge: 6 * time.Hour,
+	}))
 
 	//health route
 	app.GET("/", func(c *gin.Context){
