1cd6fe3d0fe21dbfeb8126f5c892b7b24361cf17
walonCode <mohamedlaminwalonjalloh@gmail.com>
2025-06-13 00:10:45 +0000

    [update]: finish the database connection funtion in the payment service server

--- server/payment-service/src/configs/mongo.ts
+++ server/payment-service/src/configs/mongo.ts
@@ -2,9 +2,15 @@ import mongoose from "mongoose";
 
 
 export const connectDB = async() => {
+    console.log("MongoDB connection with retry")
     try{
-        
+        await mongoose.connect(process.env.DATABASE_URI_PAYMENT!, {
+            dbName:"payment",
+        })
     }catch(err){
         console.error(err)
+        setTimeout(() => {
+            connectDB()
+        },5000)
     }
 }
