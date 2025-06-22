9b48812b62ee022546426e2d385407044c23903d
walonCode <mohamedlaminwalonjalloh@gmail.com>
2025-06-22 08:57:55 +0000

    add the bookingvalidator schema

---
+++ server/src/validators/booking.ts
@@ -0,0 +1,12 @@
+import { z } from "zod"
+
+export const bookingCreateSchema = z.object({
+    userId:z.number().min(2),
+    hotelId:z.number().min(2),
+    roomId:z.number().min(2),
+    checkIn: z.date(),
+    checkOut:z.date(),
+    totalPrice:z.number().min(2),
+    isPaid: z.boolean().optional().default(false),
+    status:z.enum(["pending","completed","cancel"]).optional().default("pending")
+})
